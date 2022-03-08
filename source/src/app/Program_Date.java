11758a020e69396bd2a5765df5330bddd523ade5
sergio-macedo <84498464+sergio-macedo@users.noreply.github.com>
2022-03-08 12:36:26 +0100

    Add files via upload
    working with the calendar

--- source/src/app/Program_Date.java
+++ source/src/app/Program_Date.java
@@ -3,27 +3,20 @@ package app;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Program_Date {
     public static void main(String[] args) throws ParseException {
-        Date x1 = new Date();
-        Date x2 = new Date(System.currentTimeMillis());
-        Date x3 = new Date(0L);
-        Date x4 = new Date(1000L*60L*60L*4L);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
 
-        SimpleDateFormat sdf1= new SimpleDateFormat("dd/MM/yyyy");
-        SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_MONTH,4);
+        date = cal.getTime();
+        System.out.println(sdf.format(date));
 
-        Date y1 = sdf1.parse("25/06/1988");
-        Date y2 = sdf2.parse("25/06/2018 15:42:07");
-        Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
-        System.out.println("x1: "+ sdf2.format(x1));
-        System.out.println("x2: "+ sdf2.format(x2));
-        System.out.println("x3: "+ sdf2.format(x3));
-        System.out.println("x3: "+ sdf2.format(x4));
-        System.out.println("y1: "+ sdf2.format(y1));
-        System.out.println("y2: "+ sdf1.format(y2));
-        System.out.println("y3: "+ sdf1.format(y3));
+
     }
 }
