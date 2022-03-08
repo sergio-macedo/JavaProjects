9db20157072703bd3da8073857f7819f6ecdf5b8
sergio-macedo <84498464+sergio-macedo@users.noreply.github.com>
2022-03-08 14:03:21 +0100

    Add files via upload
    using matrix that are doesn't have the same amount of lines and columns

--- source/src/app/Matrix.java
+++ source/src/app/Matrix.java
@@ -6,26 +6,45 @@ public class Matrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Type the dimensions of the matrix with it's lines and columns.");
         int n = sc.nextInt();
-        int[][] matrix = new int[n][n];
+        int m = sc.nextInt();
+        sc.nextLine();
+        int[][] matrix = new int[n][m];
+        sc.nextLine();
         int negNumber = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.println("type the next element of the matrix");
                 matrix[i][j] = sc.nextInt();
-                if(matrix[i][j] < 0 ){
-                    negNumber++;
+            }
+        }
+        System.out.println("find a number and its neighbors at the matrix");
+        int number = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (number == matrix[i][j]) {
+                    System.out.println("position " + i +"," + j);
+
+                    if(j > 0){
+                        System.out.println("left: " + matrix[i][j - 1]);
+                    }
+                    if(i > 0) {
+                        System.out.println("up: " + matrix[i - 1][j]);
+                    }
+                    if(j < matrix[i].length - 1 ) {
+                        System.out.println("right: " + matrix[i][j + 1]);
+
+                    }
+                    if(i < matrix.length - 1) {
+                        System.out.println("down: " + matrix[i + 1][j]);
+                    }
                 }
             }
         }
-        System.out.println("Main diagonal");
-        for (int i = 0; i < n ; i++){
-            System.out.print(matrix[i][i]+ " ");
-
-        }
-        System.out.println("the amount of negative numbers inside the matrix is" + negNumber);
-
         sc.close();
-
     }
+
 }
